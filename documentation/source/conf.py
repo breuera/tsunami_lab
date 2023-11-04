7e8bb26a02ba6baa19c31ce1030a36e47eed84b3
BohdanBabii <bohdanbabii2000@gmail.com>
2023-11-04 17:51:59 +0100

    change layout of sphinx

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -37,5 +37,24 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    
+    'logo_only': False,
+
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+# html_logo = ''
+# github_url = ''
+# html_baseurl = ''
 
 html_static_path = ['_static']
