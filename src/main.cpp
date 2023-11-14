238c1893efa4bc2746f4cd5ab82331a2fdad9526
PhillipRothenbeck <philliprothenbeck@gmail.com>
2023-11-14 13:01:28 +0100

    change Csv include

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include <limits>
 
 #include "configs/SimConfig.h"
-#include "io/Csv.h"
+#include "io/Csv/Csv.h"
 #include "patches/WavePropagation1d.h"
 #include "setups/CustomSetup1d/CustomSetup1d.h"
 #include "setups/DamBreak1d/DamBreak1d.h"
@@ -354,138 +354,6 @@ std::string l_boundary_conditions = "-oo";*/
             std::cerr << "entered SCENARIO_MODE is unknown" << std::endl;
             return EXIT_FAILURE;
         }
-
-        // construct solver
-        tsunami_lab::patches::WavePropagation *l_waveProp;
-        l_waveProp = new tsunami_lab::patches::WavePropagation1d(l_nx, l_use_roe_solver);
-
-        // maximum observed height in the setup
-        tsunami_lab::t_real l_hMax = std::numeric_limits<tsunami_lab::t_real>::lowest();
-
-        // set up solver
-        for (tsunami_lab::t_idx l_cy = 0; l_cy < l_ny; l_cy++) {
-            tsunami_lab::t_real l_y = l_cy * l_dxy;
-
-            for (tsunami_lab::t_idx l_cx = 0; l_cx < l_nx; l_cx++) {
-                tsunami_lab::t_real l_x = l_cx * l_dxy;
-
-                // get initial values of the setup
-                tsunami_lab::t_real l_h = l_setup->getHeight(l_x,
-                                                             l_y);
-                l_hMax = std::max(l_h, l_hMax);
-
-                tsunami_lab::t_real l_hu = l_setup->getMomentumX(l_x,
-                                                                 l_y);
-                tsunami_lab::t_real l_hv = l_setup->getMomentumY(l_x,
-                                                                 l_y);
-                tsunami_lab::t_real l_b = l_setup->getBathymetry(l_x,
-                                                                 l_y);
-
-                // set initial values in wave propagation solver
-                l_waveProp->setHeight(l_cx,
-                                      l_cy,
-                                      l_h);
-
-                l_waveProp->setMomentumX(l_cx,
-                                         l_cy,
-                                         l_hu);
-
-                l_waveProp->setMomentumY(l_cx,
-                                         l_cy,
-                                         l_hv);
-
-                l_waveProp->setBathymetry(l_cx,
-                                          l_b);
-            }
-        }
-
-        // derive maximum wave speed in setup; the momentum is ignored
-        tsunami_lab::t_real l_speedMax = std::sqrt(9.81 * l_hMax);
-
-        // derive constant time step; changes at simulation time are ignored
-        tsunami_lab::t_real l_dt = 0.5 * l_dxy / l_speedMax;
-
-        // derive scaling for a time step
-        tsunami_lab::t_real l_scaling = l_dt / l_dxy;
-
-        // set up time and print control
-        tsunami_lab::t_idx l_nOut = 0;
-        tsunami_lab::t_real l_endTime = 1.25;
-        tsunami_lab::t_real l_simTime = 0;
-
-        std::cout << "entering time loop" << std::endl;
-        if (!(l_scenario == "Sanitize1d")) {
-            tsunami_lab::t_idx l_timeStep = 0;
-            // iterate over time
-            while (l_simTime < l_endTime) {
-                if (l_timeStep % 25 == 0) {
-                    std::cout << "  simulation time / #time steps: "
-                              << l_simTime << " / " << l_timeStep << std::endl;
-
-                    std::string l_path = "./out/solution_" + std::to_string(l_nOut) + ".csv";
-                    std::cout << "  writing wave field to " << l_path << std::endl;
-
-                    std::ofstream l_file;
-                    l_file.open(l_path);
-
-                    tsunami_lab::io::Csv::write(l_dxy,
-                                                l_nx,
-                                                1,
-                                                1,
-                                                l_waveProp->getHeight(),
-                                                l_waveProp->getMomentumX(),
-                                                nullptr,
-                                                l_waveProp->getBathymetry(),
-                                                l_file);
-                    l_file.close();
-                    l_nOut++;
-                }
-                if (l_boundary_conditions == "-rr") {
-                    l_waveProp->setGhostReflectingBoundaryConditions();
-                } else if (l_boundary_conditions == "-or") {
-                    l_waveProp->setGhostRightReflectingBoundaryCondition();
-                } else if (l_boundary_conditions == "-ro") {
-                    l_waveProp->setGhostLeftReflectingBoundaryCondition();
-                } else {
-                    l_waveProp->setGhostOutflow();
-                }
-
-                l_waveProp->timeStep(l_scaling);
-
-                l_timeStep++;
-                l_simTime += l_dt;
-            }
-        } else {
-            tsunami_lab::t_idx l_number_of_time_steps = 100;
-            bool l_is_correct_middle_state = false;
-            for (tsunami_lab::t_idx l_timeStep = 0; l_timeStep < l_number_of_time_steps; l_timeStep++) {
-                if (l_boundary_conditions == "-rr") {
-                    l_waveProp->setGhostReflectingBoundaryConditions();
-                } else if (l_boundary_conditions == "-or") {
-                    l_waveProp->setGhostRightReflectingBoundaryCondition();
-                } else if (l_boundary_conditions == "-ro") {
-                    l_waveProp->setGhostLeftReflectingBoundaryCondition();
-                } else {
-                    l_waveProp->setGhostOutflow();
-                }
-                l_waveProp->timeStep(l_scaling);
-
-                tsunami_lab::t_real l_middle_state = l_waveProp->getHeight()[(tsunami_lab::t_idx)l_locMiddle];
-                if (abs(l_middle_state - l_hStar[l_idx]) < 4.20) {
-                    l_is_correct_middle_state = true;
-                }
-            }
-            if (l_is_correct_middle_state) {
-                std::cout << "middle stated was calculated: true" << std::endl;
-            } else {
-                std::cout << "middle stated was calculated: false" << std::endl;
-            }
-        }
-        // free memory
-        std::cout << "finished time loop" << std::endl;
-        std::cout << "freeing memory" << std::endl;
-        delete l_setup;
-        delete l_waveProp;
     }
     // free memory off scenario data
 
