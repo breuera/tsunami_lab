db033e7ac01c08575b8dab3fa11003a715d4292a
Mher Mnatsakanyan <mher.mnatsakanyan02@gmail.com>
2023-11-17 15:27:35 +0100

    moved forward with change to 2d

--- src/patches/wavepropagation2d/WavePropagation2d.cpp
+++ src/patches/wavepropagation2d/WavePropagation2d.cpp
@@ -14,141 +14,334 @@
 #include "../../solvers/f-wave/F_wave.h"
 #include "../../solvers/roe/Roe.h"
 
-tsunami_lab::patches::WavePropagation2d::WavePropagation2d(t_idx i_nCells,
+tsunami_lab::patches::WavePropagation2d::WavePropagation2d(t_idx i_nCells_x,
+                                                           t_idx i_nCells_y,
                                                            int solver_choice,
                                                            int state_boundary_left,
                                                            int state_boundary_right,
                                                            int state_boundary_top,
-                                                           int state_boundary_bottom) {
-    m_nCells = i_nCells;
+                                                           int state_boundary_bottom)
+{
+    m_nCells_x = i_nCells_x;
+    m_nCells_y = i_nCells_y;
     m_solver_choice = solver_choice;
     m_state_boundary_left = state_boundary_left;
     m_state_boundary_right = state_boundary_right;
     m_state_boundary_top = state_boundary_top;
     m_state_boundary_bottom = state_boundary_bottom;
     // allocate memory including a single ghost cell on each side and initializing with 0
-    for (int i = 0; i < 2; i++) {
-        m_h[i] = new t_real *[m_nCells + 2];
-        m_hu[i] = new t_real *[m_nCells + 2];
-        for (int j = 0; j < m_nCells + 2; j++) {
-            m_h[i][j] = new t_real[m_nCells + 2]{0};
-            m_hu[i][j] = new t_real[m_nCells + 2]{0};
+    for (int i = 0; i < 2; i++)
+    {
+        m_h[i] = new t_real *[m_nCells_x + 2];
+        m_hu[i] = new t_real *[m_nCells_x + 2];
+        m_hv[i] = new t_real *[m_nCells_x + 2];
+        for (int j = 0; j < m_nCells_x + 2; j++)
+        {
+            m_h[i][j] = new t_real[m_nCells_y + 2]{0};
+            m_hu[i][j] = new t_real[m_nCells_y + 2]{0};
+            m_hv[i][j] = new t_real[m_nCells_y + 2]{0};
         }
     }
 
-    m_b = new t_real *[m_nCells + 2];
-    for (int j = 0; j < m_nCells + 2; j++) {
-        m_b[j] = new t_real[m_nCells + 2]{0};
+    m_b = new t_real *[m_nCells_x + 2];
+    for (int j = 0; j < m_nCells_x + 2; j++)
+    {
+        m_b[j] = new t_real[m_nCells_y + 2]{0};
     }
 }
 
-tsunami_lab::patches::WavePropagation2d::~WavePropagation2d() {
-    for (unsigned short l_st = 0; l_st < 2; l_st++) {
+tsunami_lab::patches::WavePropagation2d::~WavePropagation2d()
+{
+    for (unsigned short l_st = 0; l_st < 2; l_st++)
+    {
+        for (int j = 0; j < m_nCells_x + 2; j++)
+        {
+            delete[] m_h[l_st][j];
+            delete[] m_hu[l_st][j];
+            delete[] m_hv[l_st][j];
+        }
         delete[] m_h[l_st];
         delete[] m_hu[l_st];
+        delete[] m_hv[l_st];
+    }
+
+    for (int j = 0; j < m_nCells_x + 2; j++)
+    {
+        delete[] m_b[j];
     }
     delete[] m_b;
 }
 
-void tsunami_lab::patches::WavePropagation2d::timeStep(t_real i_scaling) {
+void tsunami_lab::patches::WavePropagation2d::timeStep(t_real i_scaling)
+{
+    //
+    // X-AXIS
+    //
     // pointers to old and new data
-    t_real *l_hOld = m_h[m_step];
-    t_real *l_huOld = m_hu[m_step];
+    t_real **l_hOld = m_h[m_step];
+    t_real **l_huOld = m_hu[m_step];
+    t_real **l_hvOld = m_hv[m_step];
 
     m_step = (m_step + 1) % 2;
-    t_real *l_hNew = m_h[m_step];
-    t_real *l_huNew = m_hu[m_step];
+    t_real **l_hNew = m_h[m_step];
+    t_real **l_huNew = m_hu[m_step];
+    t_real **l_hvNew = m_hv[m_step];
 
-    t_real *l_b = m_b;
+    t_real **l_b = m_b;
 
     // init new cell quantities
-    for (t_idx l_ce = 1; l_ce < m_nCells + 1; l_ce++) {
-        l_hNew[l_ce] = l_hOld[l_ce];
-        l_huNew[l_ce] = l_huOld[l_ce];
+    for (t_idx l_x = 1; l_x < m_nCells_x + 1; l_x++)
+    {
+        for (t_idx l_y = 1; l_y < m_nCells_y + 1; l_y++)
+        {
+            l_hNew[l_x][l_y] = l_hOld[l_x][l_y];
+            l_huNew[l_x][l_y] = l_huOld[l_x][l_y];
+            l_hvNew[l_x][l_y] = l_hvOld[l_x][l_y];
+        }
     }
 
-    // iterate over edges and update with Riemann solutions
-    for (t_idx l_ed = 0; l_ed < m_nCells + 1; l_ed++) {
-        // determine left and right cell-id
-        t_idx l_ceL = l_ed;
-        t_idx l_ceR = l_ed + 1;
+    // iterate over edges and update with Riemann solutions in x-direction
+    for (t_idx l_x = 0; l_x < m_nCells_x + 1; l_x++)
+    {
+        for (t_idx l_y = 0; l_y < m_nCells_y + 1; l_y++)
+        {
+            // determine left and right cell-id
+            t_idx l_xL = l_x;
+            t_idx l_xR = l_x + 1;
 
-        // compute net-updates
-        t_real l_netUpdates[2][2];
+            // compute net-updates
+            t_real l_netUpdates[2][2];
 
-        if (m_solver_choice == 1) {
-            solvers::Roe::netUpdates(l_hOld[l_ceL],
-                                     l_hOld[l_ceR],
-                                     l_huOld[l_ceL],
-                                     l_huOld[l_ceR],
-                                     l_netUpdates[0],
-                                     l_netUpdates[1]);
-        } else if (m_solver_choice == 0) {
-            solvers::FWave::netUpdates(l_hOld[l_ceL],
-                                       l_hOld[l_ceR],
-                                       l_huOld[l_ceL],
-                                       l_huOld[l_ceR],
-                                       l_b[l_ceL],
-                                       l_b[l_ceR],
-                                       l_netUpdates[0],
-                                       l_netUpdates[1]);
-        } else {
-            throw std::invalid_argument("Not a valid solver. Try again with either 'roe' or 'fwave'.");
+            if (m_solver_choice == 1)
+            {
+                solvers::Roe::netUpdates(l_hOld[l_xL][l_y],
+                                         l_hOld[l_xR][l_y],
+                                         l_huOld[l_xL][l_y],
+                                         l_huOld[l_xR][l_y],
+                                         l_netUpdates[0],
+                                         l_netUpdates[1]);
+            }
+            else if (m_solver_choice == 0)
+            {
+                solvers::FWave::netUpdates(l_hOld[l_xL][l_y],
+                                           l_hOld[l_xR][l_y],
+                                           l_huOld[l_xL][l_y],
+                                           l_huOld[l_xR][l_y],
+                                           l_b[l_xL][l_y],
+                                           l_b[l_xR][l_y],
+                                           l_netUpdates[0],
+                                           l_netUpdates[1]);
+            }
+            else
+            {
+                throw std::invalid_argument("Not a valid solver. Try again with either 'roe' or 'fwave'.");
+            }
+
+            // update the cells' quantities
+            l_hNew[l_xL][l_y] -= i_scaling * l_netUpdates[0][0];
+            l_huNew[l_xL][l_y] -= i_scaling * l_netUpdates[0][1];
+
+            l_hNew[l_xR][l_y] -= i_scaling * l_netUpdates[1][0];
+            l_huNew[l_xR][l_y] -= i_scaling * l_netUpdates[1][1];
         }
+    }
 
-        // update the cells' quantities
-        l_hNew[l_ceL] -= i_scaling * l_netUpdates[0][0];
-        l_huNew[l_ceL] -= i_scaling * l_netUpdates[0][1];
+    //
+    // Y-AXIS
+    //
+    // pointers to old and new data
+    l_hOld = m_h[m_step];
+    l_huOld = m_hu[m_step];
+    l_hvOld = m_hv[m_step];
 
-        l_hNew[l_ceR] -= i_scaling * l_netUpdates[1][0];
-        l_huNew[l_ceR] -= i_scaling * l_netUpdates[1][1];
+    m_step = (m_step + 1) % 2;
+    l_hNew = m_h[m_step];
+    l_huNew = m_hu[m_step];
+    l_hvNew = m_hv[m_step];
+
+    l_b = m_b;
+
+    // init new cell quantities
+    for (t_idx l_x = 1; l_x < m_nCells_x + 1; l_x++)
+    {
+        for (t_idx l_y = 1; l_y < m_nCells_y + 1; l_y++)
+        {
+            l_hNew[l_x][l_y] = l_hOld[l_x][l_y];
+            l_huNew[l_x][l_y] = l_huOld[l_x][l_y];
+            l_hvNew[l_x][l_y] = l_hvOld[l_x][l_y];
+        }
+    }
+    // iterate over edges and update with Riemann solutions in y-direction
+    for (t_idx l_x = 0; l_x < m_nCells_x + 1; l_x++)
+    {
+        for (t_idx l_y = 0; l_y < m_nCells_y + 1; l_y++)
+        {
+            // determine left and right cell-id
+            t_idx l_yL = l_y;
+            t_idx l_yR = l_y + 1;
+
+            // compute net-updates
+            t_real l_netUpdates[2][2];
+
+            if (m_solver_choice == 1)
+            {
+                solvers::Roe::netUpdates(l_hOld[l_x][l_yL],
+                                         l_hOld[l_x][l_yR],
+                                         l_huOld[l_x][l_yL],
+                                         l_huOld[l_x][l_yR],
+                                         l_netUpdates[0],
+                                         l_netUpdates[1]);
+            }
+            else if (m_solver_choice == 0)
+            {
+                solvers::FWave::netUpdates(l_hOld[l_x][l_yL],
+                                           l_hOld[l_x][l_yR],
+                                           l_huOld[l_x][l_yL],
+                                           l_huOld[l_x][l_yR],
+                                           l_b[l_x][l_yL],
+                                           l_b[l_x][l_yR],
+                                           l_netUpdates[0],
+                                           l_netUpdates[1]);
+            }
+            else
+            {
+                throw std::invalid_argument("Not a valid solver. Try again with either 'roe' or 'fwave'.");
+            }
+
+            // update the cells' quantities
+            l_hNew[l_x][l_yL] -= i_scaling * l_netUpdates[0][0];
+            l_huNew[l_x][l_yL] -= i_scaling * l_netUpdates[0][1];
+
+            l_hNew[l_x][l_yR] -= i_scaling * l_netUpdates[1][0];
+            l_huNew[l_x][l_yR] -= i_scaling * l_netUpdates[1][1];
+        }
     }
 }
 
-void tsunami_lab::patches::WavePropagation2d::setGhostOutflow() {
-    t_real *l_h = m_h[m_step];
-    t_real *l_hu = m_hu[m_step];
-    t_real *l_b = m_b;
+void tsunami_lab::patches::WavePropagation2d::setGhostOutflow()
+{
+    t_real **l_h = m_h[m_step];
+    t_real **l_hu = m_hu[m_step];
+    t_real **l_hv = m_hv[m_step];
+    t_real **l_b = m_b;
 
     // set left boundary
-    switch (m_state_boundary_left) {
-        // open
-        case 0:
-            l_h[0] = l_h[1];
-            l_hu[0] = l_hu[1];
-            l_b[0] = l_b[1];
-            break;
-        // closed
-        case 1:
-            l_h[0] = l_h[1];
-            l_hu[0] = -l_hu[1];
-            l_b[0] = l_b[1];
-            break;
+    switch (m_state_boundary_left)
+    {
+    // open
+    case 0:
+        for (t_idx l_y = 0; l_y < m_nCells_y; l_y++)
+        {
+            l_h[0][l_y] = l_h[1][l_y];
+            l_hu[0][l_y] = l_hu[1][l_y];
+            l_hv[0][l_y] = l_hv[1][l_y];
+            l_b[0][l_y] = l_b[1][l_y];
+        }
+        break;
+    // closed
+    case 1:
+        for (t_idx l_y = 0; l_y < m_nCells_y; l_y++)
+        {
+            l_h[0][l_y] = l_h[1][l_y];
+            l_hu[0][l_y] = -l_hu[1][l_y];
+            l_hv[0][l_y] = l_hv[1][l_y];
+            l_b[0][l_y] = l_b[1][l_y];
+        }
+        break;
 
-        default:
-            std::cerr << "undefined state for left boundary" << std::endl;
-            exit(EXIT_FAILURE);
-            break;
+    default:
+        std::cerr << "undefined state for left boundary" << std::endl;
+        exit(EXIT_FAILURE);
+        break;
     }
 
     // set right boundary
-    switch (m_state_boundary_right) {
-        // open
-        case 0:
-            l_h[m_nCells + 1] = l_h[m_nCells];
-            l_hu[m_nCells + 1] = l_hu[m_nCells];
-            l_b[m_nCells + 1] = l_b[m_nCells];
-            break;
-        // closed
-        case 1:
-            l_h[m_nCells + 1] = l_h[m_nCells];
-            l_hu[m_nCells + 1] = -l_hu[m_nCells];
-            l_b[m_nCells + 1] = l_b[m_nCells];
-            break;
+    switch (m_state_boundary_right)
+    {
+    // open
+    case 0:
+        for (t_idx l_y = 0; l_y < m_nCells_y; l_y++)
+        {
+            l_h[m_nCells_x + 1][l_y] = l_h[m_nCells_x][l_y];
+            l_hu[m_nCells_x + 1][l_y] = l_hu[m_nCells_x][l_y];
+            l_hv[m_nCells_x + 1][l_y] = l_hv[m_nCells_x][l_y];
+            l_b[m_nCells_x + 1][l_y] = l_b[m_nCells_x][l_y];
+        }
+        break;
+    // closed
+    case 1:
+        for (t_idx l_y = 0; l_y < m_nCells_y; l_y++)
+        {
+            l_h[m_nCells_x + 1][l_y] = l_h[m_nCells_x][l_y];
+            l_hu[m_nCells_x + 1][l_y] = -l_hu[m_nCells_x][l_y];
+            l_hv[m_nCells_x + 1][l_y] = l_hv[m_nCells_x][l_y];
+            l_b[m_nCells_x + 1][l_y] = l_b[m_nCells_x][l_y];
+        }
+        break;
 
-        default:
-            std::cerr << "undefined state for right boundary" << std::endl;
-            exit(EXIT_FAILURE);
-            break;
+    default:
+        std::cerr << "undefined state for right boundary" << std::endl;
+        exit(EXIT_FAILURE);
+        break;
+    }
+
+    // set top boundary
+    switch (m_state_boundary_top)
+    {
+    // open
+    case 0:
+        for (t_idx l_x = 0; l_x < m_nCells_x; l_x++)
+        {
+            l_h[l_x][0] = l_h[l_x][1];
+            l_hu[l_x][0] = l_hu[l_x][1];
+            l_hv[l_x][0] = l_hv[l_x][1];
+            l_b[l_x][0] = l_b[l_x][1];
+        }
+        break;
+    // closed
+    case 1:
+        for (t_idx l_x = 0; l_x < m_nCells_x; l_x++)
+        {
+            l_h[l_x][0] = l_h[l_x][1];
+            l_hu[l_x][0] = l_hu[l_x][1];
+            l_hv[l_x][0] = -l_hv[l_x][1];
+            l_b[l_x][0] = l_b[l_x][1];
+        }
+        break;
+
+    default:
+        std::cerr << "undefined state for top boundary" << std::endl;
+        exit(EXIT_FAILURE);
+        break;
+    }
+
+    // set bottom boundary
+    switch (m_state_boundary_bottom)
+    {
+    // open
+    case 0:
+        for (t_idx l_x = 0; l_x < m_nCells_x; l_x++)
+        {
+            l_h[l_x][m_nCells_y + 1] = l_h[l_x][m_nCells_y];
+            l_hu[l_x][m_nCells_y + 1] = l_hu[l_x][m_nCells_y];
+            l_hv[l_x][m_nCells_y + 1] = l_hv[l_x][m_nCells_y];
+            l_b[l_x][m_nCells_y + 1] = l_b[l_x][m_nCells_y];
+        }
+        break;
+    // closed
+    case 1:
+        for (t_idx l_x = 0; l_x < m_nCells_x; l_x++)
+        {
+            l_h[l_x][m_nCells_y + 1] = l_h[l_x][m_nCells_y];
+            l_hu[l_x][m_nCells_y + 1] = l_hu[l_x][m_nCells_y];
+            l_hv[l_x][m_nCells_y + 1] = -l_hv[l_x][m_nCells_y];
+            l_b[l_x][m_nCells_y + 1] = l_b[l_x][m_nCells_y];
+        }
+        break;
+
+    default:
+        std::cerr << "undefined state for bottom boundary" << std::endl;
+        exit(EXIT_FAILURE);
+        break;
     }
 }
--- src/patches/wavepropagation2d/WavePropagation2d.h
+++ src/patches/wavepropagation2d/WavePropagation2d.h
@@ -13,19 +13,25 @@
 
 #include "../WavePropagation.h"
 
-namespace tsunami_lab {
-namespace patches {
-class WavePropagation2d;
-}
-}  // namespace tsunami_lab
+namespace tsunami_lab
+{
+    namespace patches
+    {
+        class WavePropagation2d;
+    }
+} // namespace tsunami_lab
 
-class tsunami_lab::patches::WavePropagation2d : public WavePropagation {
-   private:
+class tsunami_lab::patches::WavePropagation2d : public WavePropagation
+{
+private:
     //! current step which indicates the active values in the arrays below
     unsigned short m_step = 0;
 
-    //! number of cells discretizing the computational domain
-    t_idx m_nCells = 0;
+    //! number of cells in x-direction discretizing the computational domain
+    t_idx m_nCells_x = 0;
+
+    //! number of cells in y-direction discretizing the computational domain
+    t_idx m_nCells_y = 0;
 
     //! choice of solver, 0 = roe, 1 = fwave
     int m_solver_choice = 0;
@@ -45,25 +51,29 @@ class tsunami_lab::patches::WavePropagation2d : public WavePropagation {
     //! water heights for the current and next time step for all cells
     // t_real *m_h[2] = {nullptr, nullptr};
     t_real **m_h[2];
-    //! momenta for the current and next time step for all cells
+    //! momenta for the current and next time step for all cells in x-direction
     t_real **m_hu[2];
+    //! momenta for the current and next time step for all cells in y-direction
+    t_real **m_hv[2];
 
     //! bathymetry for all cells
     t_real **m_b;
 
-   public:
+public:
     /**
      *
      * Constructs the 1d wave propagation solver.
      *
-     * @param i_nCells number of cells.
+     * @param i_nCells_x number of cells in x-direction.
+     * @param i_nCells_y number of cells in y-direction.
      * @param solver_choice type int, defines the choice of solver. Possible values: 0 for "roe" and 1 for "fwave".
      * @param state_boundary_left type int, defines the state of the left boundary. Possible values: 0 = "open" and 1 = "closed".
      * @param state_boundary_right type int, defines the state of the right boundary. Possible values: 0 = "open" and 1 = "closed".
      * @param state_boundary_top type int, defines the state of the top boundary. Possible values: 0 = "open" and 1 = "closed".
      * @param state_boundary_bottom type int, defines the state of the bottom boundary. Possible values: 0 = "open" and 1 = "closed".
      **/
-    WavePropagation2d(t_idx i_nCells,
+    WavePropagation2d(t_idx i_nCells_x,
+                      t_idx i_nCells_y,
                       int solver_choice,
                       int state_boundary_left,
                       int state_boundary_right,
@@ -92,8 +102,9 @@ class tsunami_lab::patches::WavePropagation2d : public WavePropagation {
      *
      * @return stride in y-direction.
      **/
-    t_idx getStride() {
-        return m_nCells + 2;
+    t_idx getStride()
+    {
+        return m_nCells_y + 2;
     }
 
     /**
@@ -101,7 +112,8 @@ class tsunami_lab::patches::WavePropagation2d : public WavePropagation {
      *
      * @return water heights.
      */
-    t_real const *getHeight() {
+    t_real const *getHeight()
+    {
         return m_h[m_step] + 1;
     }
 
@@ -110,15 +122,17 @@ class tsunami_lab::patches::WavePropagation2d : public WavePropagation {
      *
      * @return momenta in x-direction.
      **/
-    t_real const *getMomentumX() {
+    t_real const *getMomentumX()
+    {
         return m_hu[m_step] + 1;
     }
 
     /**
      * Dummy function which returns a nullptr.
      **/
-    t_real const *getMomentumY() {
-        return nullptr;
+    t_real const *getMomentumY()
+    {
+        return m_hv[m_step] + 1;
     }
 
     /**
@@ -126,7 +140,8 @@ class tsunami_lab::patches::WavePropagation2d : public WavePropagation {
      *
      * @return bathymetry.
      **/
-    t_real const *getBathymetry() {
+    t_real const *getBathymetry()
+    {
         return m_b + 1;
     }
 
@@ -134,43 +149,52 @@ class tsunami_lab::patches::WavePropagation2d : public WavePropagation {
      * Sets the height of the cell to the given value.
      *
      * @param i_ix id of the cell in x-direction.
+     * @param i_iy id of the cell in y-direction.
      * @param i_h water height.
      **/
     void setHeight(t_idx i_ix,
-                   t_idx,
-                   t_real i_h) {
-        m_h[m_step][i_ix + 1] = i_h;
+                   t_idx i_iy,
+                   t_real i_h)
+    {
+        m_h[m_step][i_ix + 1][i_iy + 1] = i_h;
     }
 
     /**
      * Sets the momentum in x-direction to the given value.
      *
      * @param i_ix id of the cell in x-direction.
+     * @param i_iy id of the cell in y-direction.
      * @param i_hu momentum in x-direction.
      **/
     void setMomentumX(t_idx i_ix,
-                      t_idx,
-                      t_real i_hu) {
-        m_hu[m_step][i_ix + 1] = i_hu;
+                      t_idx i_iy,
+                      t_real i_hu)
+    {
+        m_hu[m_step][i_ix + 1][i_iy + 1] = i_hu;
     }
 
     /**
      * Dummy function since there is no y-momentum in the 1d solver.
      **/
-    void setMomentumY(t_idx,
-                      t_idx,
-                      t_real){};
+    void setMomentumY(t_idx i_ix,
+                      t_idx i_iy,
+                      t_real i_hv)
+    {
+        m_hv[m_step][i_ix + 1][i_iy + 1] = i_hv;
+    }
 
     /**
      * @brief Set the bathymetry
      *
      * @param i_ix id of the cell in x-direction.
+     * @param i_iy id of the cell in y-direction.
      * @param i_b bathymetry
      */
     void setBathymetry(t_idx i_ix,
-                       t_idx,
-                       t_real i_b) {
-        m_b[i_ix + 1] = i_b;
+                       t_idx i_iy,
+                       t_real i_b)
+    {
+        m_b[i_ix + 1][i_iy + 1] = i_b;
     }
 };
 
