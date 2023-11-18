6a2af1e0f2e0315d15e1b3c311824a6f75e85184
PhillipRothenbeck <philliprothenbeck@gmail.com>
2023-11-18 21:44:46 +0100

    fix indentation

--- src/solvers/FWave.h
+++ src/solvers/FWave.h
@@ -5,131 +5,128 @@
  * F Wave solver for the one-dimensional shallow water equations.
  **/
 
-#ifndef TSUNAMI_LAB_SOLVERS_F_WAFE
-#define TSUNAMI_LAB_SOLVERS_F_WAFE
+#ifndef TSUNAMI_LAB_SOLVERS_F_WAVE
+#define TSUNAMI_LAB_SOLVERS_F_WAVE
 
 #include "../constants.h"
 
-namespace tsunami_lab
-{
-	namespace solvers
-	{
-		class FWave;
-	}
-}
+namespace tsunami_lab {
+    namespace solvers {
+        class FWave;
+    }
+}  // namespace tsunami_lab
 
-class tsunami_lab::solvers::FWave
-{
-private:
-	//! gravity constant
-	static t_real constexpr c_g = 9.80665;
+class tsunami_lab::solvers::FWave {
+   private:
+    //! gravity constant
+    static t_real constexpr c_g = 9.80665;
 
-	//! square root of gravity
-	static t_real constexpr c_sqrt_g = 3.131557121;
+    //! square root of gravity
+    static t_real constexpr c_sqrt_g = 3.131557121;
 
-	/**
-	 * Computes the average wave height.
-	 *
-	 * @param i_hL height of the left side.
-	 * @param i_hR height of the right side.
-	 * @param o_hight will be set to the average speed.
-	 **/
+    /**
+     * Computes the average wave height.
+     *
+     * @param i_hL height of the left side.
+     * @param i_hR height of the right side.
+     * @param o_hight will be set to the average speed.
+     **/
 
-	static void heightAvg(t_real i_hL,
-						  t_real i_hR,
-						  t_real &o_height);
+    static void heightAvg(t_real i_hL,
+                          t_real i_hR,
+                          t_real &o_height);
 
-	/**
-	 * Computes the average particle_Velocity
-	 *
-	 * @param i_hL height of the left side.
-	 * @param i_hR height of the right side.
-	 * @param i_huL momentum of the left side.
-	 * @param i_huR momentum of the right side.
-	 * @param o_velocity will be set to the average velocity.
-	 **/
+    /**
+     * Computes the average particle_Velocity
+     *
+     * @param i_hL height of the left side.
+     * @param i_hR height of the right side.
+     * @param i_huL momentum of the left side.
+     * @param i_huR momentum of the right side.
+     * @param o_velocity will be set to the average velocity.
+     **/
 
-	static void particleVelocityAvg(t_real i_hL,
-									t_real i_hR,
-									t_real i_uL,
-									t_real i_uR,
-									t_real &o_velocity);
-	/**
-	 * Computes the wave speeds.
-	 *
-	 * @param i_hL height of the left side.
-	 * @param i_hR height of the right side.
-	 * @param i_huL momentum of the left side.
-	 * @param i_huR momentum of the right side.
-	 * @param o_waveSpeedL will be set to the speed of the wave propagating to the left.
-	 * @param o_waveSpeedR will be set to the speed of the wave propagating to the right.
-	 **/
+    static void particleVelocityAvg(t_real i_hL,
+                                    t_real i_hR,
+                                    t_real i_uL,
+                                    t_real i_uR,
+                                    t_real &o_velocity);
+    /**
+     * Computes the wave speeds.
+     *
+     * @param i_hL height of the left side.
+     * @param i_hR height of the right side.
+     * @param i_huL momentum of the left side.
+     * @param i_huR momentum of the right side.
+     * @param o_waveSpeedL will be set to the speed of the wave propagating to the left.
+     * @param o_waveSpeedR will be set to the speed of the wave propagating to the right.
+     **/
 
-	static void waveSpeeds(t_real i_hL,
-						   t_real i_hR,
-						   t_real i_uL,
-						   t_real i_uR,
-						   t_real &o_waveSpeedL,
-						   t_real &o_waveSpeedR);
+    static void waveSpeeds(t_real i_hL,
+                           t_real i_hR,
+                           t_real i_uL,
+                           t_real i_uR,
+                           t_real &o_waveSpeedL,
+                           t_real &o_waveSpeedR);
 
-	/**
-	 * Computes the flux function for one cell.
-	 *
-	 * @param i_h height of the cell
-	 * @param i_hu momentum of the cell
-	 * @param o_flux will be set to the flux function value
-	 **/
-	static void flux(t_real i_h,
-					 t_real i_hu,
-					 t_real o_flux[2]);
+    /**
+     * Computes the flux function for one cell.
+     *
+     * @param i_h height of the cell
+     * @param i_hu momentum of the cell
+     * @param o_flux will be set to the flux function value
+     **/
+    static void flux(t_real i_h,
+                     t_real i_hu,
+                     t_real o_flux[2]);
 
-	/**
-	 * Computes the wave strengths
-	 *
-	 * @param i_hL height of the left side.
-	 * @param i_hR height of the right side.
-	 * @param i_huL momentum of the left side.
-	 * @param i_huR momentum of the right side.
-	 * @param i_bL bathymetry of the left side.
-	 * @param i_bR bathymetry of the right side.
-	 * @param i_waveSpeedL wave speed of the left side.
-	 * @param i_waveSpeedR wave speed of the right side.
-	 * @param o_strengthL will be set to the strength of the wave propagation to the left.
-	 * @param o_strengthR will be set to the strength of the wave propagation to the right.
-	 **/
+    /**
+     * Computes the wave strengths
+     *
+     * @param i_hL height of the left side.
+     * @param i_hR height of the right side.
+     * @param i_huL momentum of the left side.
+     * @param i_huR momentum of the right side.
+     * @param i_bL bathymetry of the left side.
+     * @param i_bR bathymetry of the right side.
+     * @param i_waveSpeedL wave speed of the left side.
+     * @param i_waveSpeedR wave speed of the right side.
+     * @param o_strengthL will be set to the strength of the wave propagation to the left.
+     * @param o_strengthR will be set to the strength of the wave propagation to the right.
+     **/
 
-	static void waveStrengths( t_real i_hL,
-							   t_real i_hR,
-							   t_real i_huL,
-							   t_real i_huR,
-							   t_real i_bL,
-							   t_real i_bR,
-							   t_real i_waveSpeedL,
-							   t_real i_waveSpeedR,
-							   t_real &o_strengthL,
-							   t_real &o_strengthR);
+    static void waveStrengths(t_real i_hL,
+                              t_real i_hR,
+                              t_real i_huL,
+                              t_real i_huR,
+                              t_real i_bL,
+                              t_real i_bR,
+                              t_real i_waveSpeedL,
+                              t_real i_waveSpeedR,
+                              t_real &o_strengthL,
+                              t_real &o_strengthR);
 
-public:
-	/**
-	 * Computes the net-updates.
-	 *
-	 * @param i_hL height of the left side.
-	 * @param i_hR height of the right side.
-	 * @param i_huL momentum of the left side.
-	 * @param i_huR momentum of the right side.
-	 * @param i_bL bathymetry of the left side.
-	 * @param i_bR bathymetry of the right side.
-	 * @param o_netUpdateL will be set to the net-updates for the left side; 0: hight 1: momentum.
-	 * @param o_netUpdateR will be set to the net-updates for the right side; 0: hight, 1: momentum.
-	 **/
+   public:
+    /**
+     * Computes the net-updates.
+     *
+     * @param i_hL height of the left side.
+     * @param i_hR height of the right side.
+     * @param i_huL momentum of the left side.
+     * @param i_huR momentum of the right side.
+     * @param i_bL bathymetry of the left side.
+     * @param i_bR bathymetry of the right side.
+     * @param o_netUpdateL will be set to the net-updates for the left side; 0: hight 1: momentum.
+     * @param o_netUpdateR will be set to the net-updates for the right side; 0: hight, 1: momentum.
+     **/
 
-	static void netUpdates(t_real i_hL,
-						   t_real i_hR,
-						   t_real i_huL,
-						   t_real i_huR,
-						   t_real i_bL,
+    static void netUpdates(t_real i_hL,
+                           t_real i_hR,
+                           t_real i_huL,
+                           t_real i_huR,
+                           t_real i_bL,
                            t_real i_bR,
-						   t_real o_netUpdateL[2],
-						   t_real o_netUpdateR[2]);
+                           t_real o_netUpdateL[2],
+                           t_real o_netUpdateR[2]);
 };
 #endif
--- src/solvers/Roe.h
+++ src/solvers/Roe.h
@@ -10,13 +10,13 @@
 #include "../constants.h"
 
 namespace tsunami_lab {
-  namespace solvers {
-    class Roe;
-  }
-}
+    namespace solvers {
+        class Roe;
+    }
+}  // namespace tsunami_lab
 
 class tsunami_lab::solvers::Roe {
-  private:
+   private:
     //! square root of gravity
     static t_real constexpr m_gSqrt = 3.131557121;
 
@@ -30,12 +30,12 @@ class tsunami_lab::solvers::Roe {
      * @param o_waveSpeedL will be set to the speed of the wave propagating to the left.
      * @param o_waveSpeedR will be set to the speed of the wave propagating to the right.
      **/
-    static void waveSpeeds( t_real   i_hL,
-                            t_real   i_hR,
-                            t_real   i_uL,
-                            t_real   i_uR,
-                            t_real & o_waveSpeedL,
-                            t_real & o_waveSpeedR );
+    static void waveSpeeds(t_real i_hL,
+                           t_real i_hR,
+                           t_real i_uL,
+                           t_real i_uR,
+                           t_real& o_waveSpeedL,
+                           t_real& o_waveSpeedR);
 
     /**
      * Computes the wave strengths.
@@ -49,16 +49,16 @@ class tsunami_lab::solvers::Roe {
      * @param o_strengthL will be set to the strength of the wave propagating to the left.
      * @param o_strengthR will be set to the strength of the wave propagating to the right.
      **/
-    static void waveStrengths( t_real   i_hL,
-                               t_real   i_hR,
-                               t_real   i_huL,
-                               t_real   i_huR,
-                               t_real   i_waveSpeedL,
-                               t_real   i_waveSpeedR,
-                               t_real & o_strengthL,
-                               t_real & o_strengthR );
+    static void waveStrengths(t_real i_hL,
+                              t_real i_hR,
+                              t_real i_huL,
+                              t_real i_huR,
+                              t_real i_waveSpeedL,
+                              t_real i_waveSpeedR,
+                              t_real& o_strengthL,
+                              t_real& o_strengthR);
 
-  public:
+   public:
     /**
      * Computes the net-updates.
      *
@@ -69,12 +69,12 @@ class tsunami_lab::solvers::Roe {
      * @param o_netUpdateL will be set to the net-updates for the left side; 0: height, 1: momentum.
      * @param o_netUpdateR will be set to the net-updates for the right side; 0: height, 1: momentum.
      **/
-    static void netUpdates( t_real i_hL,
-                            t_real i_hR,
-                            t_real i_huL,
-                            t_real i_huR,
-                            t_real o_netUpdateL[2],
-                            t_real o_netUpdateR[2] );
+    static void netUpdates(t_real i_hL,
+                           t_real i_hR,
+                           t_real i_huL,
+                           t_real i_huR,
+                           t_real o_netUpdateL[2],
+                           t_real o_netUpdateR[2]);
 };
 
 #endif
