83f427df7ba15fe4e38ed544fdf85e09c2a8af0b
PhillipRothenbeck <philliprothenbeck@gmail.com>
2023-11-27 15:54:16 +0100

    rewrite ArtificialTsunamiEvent2d

--- src/constants.h
+++ src/constants.h
@@ -6,15 +6,16 @@
  **/
 #ifndef TSUNAMI_LAB_CONSTANTS_H
 #define TSUNAMI_LAB_CONSTANTS_H
+#define C_PI 3.14159265358979323846 /* pi */
 
 #include <cstddef>
 
 namespace tsunami_lab {
-  //! integral type for cell-ids, pointer arithmetic, etc.
-  typedef std::size_t t_idx;
+    //! integral type for cell-ids, pointer arithmetic, etc.
+    typedef std::size_t t_idx;
 
-  //! floating point type
-  typedef float t_real;
-}
+    //! floating point type
+    typedef float t_real;
+}  // namespace tsunami_lab
 
 #endif
--- src/setups/ArtificialTsunami2d/ArtificialTsunami2d.cpp
+++ src/setups/ArtificialTsunami2d/ArtificialTsunami2d.cpp
@@ -12,16 +12,19 @@
 #include <cmath>
 #include <iostream>
 
-tsunami_lab::setups::ArtificialTsunami2d::ArtificialTsunami2d(t_real in_simLenX,
-                                                              t_real in_simLenY) {
-    m_simLenX = in_simLenX;
-    m_simLenY = in_simLenY;
+tsunami_lab::setups::ArtificialTsunami2d::ArtificialTsunami2d(t_real i_simLenX,
+                                                              t_real i_simLenY) {
+    m_simLenX = i_simLenX;
+    m_simLenY = i_simLenY;
 }
 
 tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getHeight(t_real,
                                                                         t_real) const {
-    // max(-bathymetry, delta(20m)) -> bathymetry constant -100 -> height constant 100
-    return 100;
+    if (m_bIn < 0) {
+        return std::max((-m_bIn), m_delta);
+    } else {
+        return 0;
+    }
 }
 
 tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getMomentumX(t_real,
@@ -34,21 +37,28 @@ tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getMomentumY(t_rea
     return 0;
 }
 
-tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getBathymetry(t_real in_x,
-                                                                            t_real in_y) const {
-    t_real displacement = 0;
-    t_real pi = 3.14159265358979323846;
-    // displacement = 5 * g(x) * f(y)
-    displacement = 5 * sin(((in_x / 500) + 1) * pi) * (-((in_y / 500) * (in_y / 500)) + 1);
+tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getBathymetry(t_real i_x,
+                                                                            t_real i_y) const {
+    t_real l_offsetX = -(m_simLenX / 2);
+    t_real l_offsetY = -(m_simLenY / 2);
 
-    // theory: 10km x 10km -> mid at 5000m. if |midx - in_x| <= 500 return -100 + displacement, else -100
+    t_real l_dispDomainStart = -((m_simLenX * 0.1) / 2);
+    t_real l_dispDomainEnd = ((m_simLenX * 0.1) / 2);
 
-    t_real midx = (m_simLenX / 2);
-    t_real midy = (m_simLenY / 2);
+    bool l_isInDomainX = (i_x + l_offsetX) > l_dispDomainStart && (i_x + l_offsetX) < l_dispDomainEnd;
+    bool l_isInDomainY = (i_y + l_offsetY) > l_dispDomainStart && (i_y + l_offsetY) < l_dispDomainEnd;
+    bool l_setDisplacement = l_isInDomainX && l_isInDomainY;
+    std::cout << (i_x + l_offsetX) << ", " << i_y + l_offsetY << ", " << l_isInDomainX << "/" << l_isInDomainY << std::endl;
+    t_real l_dxy = 0;
+    if (l_setDisplacement) {
+        t_real l_fx = std::sin(((i_x / 500) + 1) * C_PI);
+        t_real l_gx = -std::pow((i_y / 500), 2) + 1;
+        l_dxy = 5 * l_fx * l_gx;
+    }
 
-    if (abs(midx - in_x) <= 500 && abs(midy - in_y) <= 500) {
-        return (-100 + displacement);
+    if (m_bIn < 0) {
+        return std::min(m_bIn, -m_delta) + l_dxy;
     } else {
-        return -100;
+        return std::max(m_bIn, m_delta) + l_dxy;
     }
 }
