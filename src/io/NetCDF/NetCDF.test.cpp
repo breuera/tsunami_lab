526a67b41d5bd5fcf41f58016723380a8d026b62
darafsa <marek1999@online.de>
2023-11-28 14:24:00 +0100

    writer test written data

--- src/io/NetCDF/NetCDF.test.cpp
+++ src/io/NetCDF/NetCDF.test.cpp
@@ -40,6 +40,7 @@ TEST_CASE("Test the NetCDF writer.", "[NetCDFWrite]") {
     int l_varIDx, l_varIDy, l_varIDtime, l_varIDheight, l_varIDmomentumX, l_varIDmomentumY, l_varIDbathymetry;
 
     REQUIRE(nc_open("./out/solution.nc", NC_NOWRITE, &l_ncId) == NC_NOERR);
+
     REQUIRE(nc_inq_varid(l_ncId, "x", &l_varIDx) == NC_NOERR);
     REQUIRE(nc_inq_varid(l_ncId, "y", &l_varIDy) == NC_NOERR);
     REQUIRE(nc_inq_varid(l_ncId, "time", &l_varIDtime) == NC_NOERR);
@@ -47,6 +48,43 @@ TEST_CASE("Test the NetCDF writer.", "[NetCDFWrite]") {
     REQUIRE(nc_inq_varid(l_ncId, "momentum_x", &l_varIDmomentumX) == NC_NOERR);
     REQUIRE(nc_inq_varid(l_ncId, "momentum_y", &l_varIDmomentumY) == NC_NOERR);
     REQUIRE(nc_inq_varid(l_ncId, "bathymetry", &l_varIDbathymetry) == NC_NOERR);
+
+    float l_dataX[2] = {0.5, 1.5};
+    float l_dataY[2] = {0.5, 1.5};
+    float l_dataTime[2] = {0.5, 1};
+    float l_dataHeight[8] = {2, 4, 6, 8, 5, 5, 5, 5};
+    float l_dataMomentumX[8] = {1, 2, 3, 4, 4, 3, 2, 1};
+    float l_dataMomentumY[8] = {1, 2, 3, 4, 4, 3, 2, 1};
+    float l_dataBathymetry[4] = {1, 2, 3, 4};
+
+    float l_resultX[2];
+    float l_resultY[2];
+    float l_resultTime[2];
+    float l_resultHeight[8];
+    float l_resultMomentumX[8];
+    float l_resultMomentumY[8];
+    float l_resultBathymetry[4];
+
+    REQUIRE(nc_get_var_float(l_ncId, l_varIDx, l_resultX) == NC_NOERR);
+    for (int i = 0; i < 2; i++) REQUIRE(l_dataX[i] == l_resultX[i]);
+
+    REQUIRE(nc_get_var_float(l_ncId, l_varIDy, l_resultY) == NC_NOERR);
+    for (int i = 0; i < 2; i++) REQUIRE(l_dataY[i] == l_resultY[i]);
+
+    REQUIRE(nc_get_var_float(l_ncId, l_varIDtime, l_resultTime) == NC_NOERR);
+    for (int i = 0; i < 2; i++) REQUIRE(l_dataTime[i] == l_resultTime[i]);
+
+    REQUIRE(nc_get_var_float(l_ncId, l_varIDheight, l_resultHeight) == NC_NOERR);
+    for (int i = 0; i < 2; i++) REQUIRE(l_dataHeight[i] == l_resultHeight[i]);
+
+    REQUIRE(nc_get_var_float(l_ncId, l_varIDmomentumX, l_resultMomentumX) == NC_NOERR);
+    for (int i = 0; i < 2; i++) REQUIRE(l_dataMomentumX[i] == l_resultMomentumX[i]);
+
+    REQUIRE(nc_get_var_float(l_ncId, l_varIDmomentumY, l_resultMomentumY) == NC_NOERR);
+    for (int i = 0; i < 2; i++) REQUIRE(l_dataMomentumY[i] == l_resultMomentumY[i]);
+
+    REQUIRE(nc_get_var_float(l_ncId, l_varIDbathymetry, l_resultBathymetry) == NC_NOERR);
+    for (int i = 0; i < 2; i++) REQUIRE(l_dataBathymetry[i] == l_resultBathymetry[i]);
 
     delete l_writer;
 }
